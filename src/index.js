96884b77cc62911333e09ae0c47e1c50d7200acd
Race Carpenter <racecarpenter@gmail.com>
2018-05-23 13:45:14 -0700

    github pages

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import Store from './store'
 const StoreInstance = Store()
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={StoreInstance}>
         <App />
     </Provider>
-  </BrowserRouter>, document.getElementById('root'));
+  </HashRouter>, document.getElementById('root'));
